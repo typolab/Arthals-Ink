f80347d45008c2d36bf3e17c4cdb898a7b861ed2
typolab <oicsci@gmail.com>
2025-12-03 19:25:50 +0800

    Update date.ts

--- packages/pure/utils/date.ts
+++ packages/pure/utils/date.ts
@@ -1,3 +1,5 @@
+<!--
+
 import config from 'virtual:config'
 
 const dateFormat = new Intl.DateTimeFormat(config.locale.dateLocale, config.locale.dateOptions)
@@ -14,4 +16,30 @@ export function getFormattedDate(
   }
 
   return dateFormat.format(new Date(date))
+}
+-->
+
+  import config from 'virtual:config'
+
+const dateFormat = new Intl.DateTimeFormat(
+  config.locale.dateLocale,
+  config.locale.dateOptions as Intl.DateTimeFormatOptions
+)
+
+export function getFormattedDate(
+  date: string | number | Date,
+  options?: Intl.DateTimeFormatOptions
+) {
+  const d = new Date(date)
+
+  // ✅ 不管是否传 options，都用 DateTimeFormat.format（能输出时间）
+  const fmt = new Intl.DateTimeFormat(
+    config.locale.dateLocale,
+    {
+      ...(config.locale.dateOptions as Intl.DateTimeFormatOptions),
+      ...(options ?? {})
+    }
+  )
+
+  return fmt.format(d)
 }
